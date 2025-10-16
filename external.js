387af82218003de6f3113164b4e8415351ac20dc
HankHall512 <hankfuereone@gmail.com>
2025-10-16 15:06:29 -0500

    Update external.js
    Added midpoint that displays the distance measurement

--- external.js
+++ external.js
@@ -43,5 +43,10 @@ L.marker([29.89331410619119, -97.94184832292025]).addTo(map)
 L.marker([29.8977314507243, -97.97082322925434]).addTo(map)
     .bindPopup("Dive Shop");
 
-document.getElementById("distance-display").innerHTML =
-  "Distance from Don's Japanese Kitchen to Dive Shop: " + distance.toFixed(2) + " miles";
+var midpoint = turf.midpoint(
+  [-97.94184832292025, 29.89331410619119],
+  [-97.97082322925434, 29.8977314507243]
+);
+
+var midpointLayer = L.geoJSON(midpoint).addTo(map);
+midpointLayer.bindPopup("Distance: " + distance.toFixed(2) + " miles");
