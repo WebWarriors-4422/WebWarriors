3ef1186954fa6e7043e92280fb359561ae73f430
HankHall512 <hankfuereone@gmail.com>
2025-10-16 14:51:07 -0500

    Update external.js
    Fixed tile layer

--- external.js
+++ external.js
@@ -2,7 +2,7 @@ var map = L.map('map').setView([29.8884, -97.9384], 14);
 mapLink =
     '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer(
-    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; ' + mapLink + ' Contributors',
     maxZoom: 18,
     }).addTo(map);
