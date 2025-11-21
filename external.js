d210292e204e4e945435ea23b8834fb6f1e11c42
Tori Haddox <tiredextras@gmail.com>
2025-11-20 20:54:45 -0600

    changed line color and updated Bishop lat/long

--- external.js
+++ external.js
@@ -45,13 +45,13 @@ fetch('City_Limits.geojson')
 
 //Bishop St. line code//
 
-var start = turf.point([-97.94576322327491, 29.884341192968666]);
-var end = turf.point([-97.92890853609376, 29.894266669878704]);
+var start = turf.point([-97.9582455, 29.8768540]);
+var end = turf.point([-97.9660008, 29.8838328]);
 var bishopLine = L.polyline([
-    [29.88434119296866, -97.94576322327491],
-    [29.89426669878704, -97.92890853609376]
+    [29.8768540, -97.9582455],
+    [29.8838328, -97.9660008]
 ], {
-    color: 'red',
+    color: 'purple',
     weight: 3
 }).addTo(map);
 
